a68bebdbac78f63d775a03c15ebb6c1019e8d84a
Muhammad Alvi Awliya <muhammadalviawliya@gmail.com>
2024-05-13 22:36:49 +0700

    Add : Theme Color & TextStyle

---
+++ lib/theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Color
+Color purpleColor = const Color(0xff5843BE);
+Color orangeColor = const Color(0xffFF9376);
+Color blackColor = Colors.black;
+Color whiteColor = Colors.white;
+Color greyColor = const Color(0xff82868E);
+
+//TextStyle
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: blackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: whiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: greyColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: purpleColor,
+);
